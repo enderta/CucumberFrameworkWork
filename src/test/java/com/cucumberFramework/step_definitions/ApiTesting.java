7769223c17625c83cddadf7f547bf0c5b116c073
enderta <endertanver@gmail.com>
2023-06-08 18:17:04 +0100

    spaceX

--- src/test/java/com/cucumberFramework/step_definitions/ApiTesting.java
+++ src/test/java/com/cucumberFramework/step_definitions/ApiTesting.java
@@ -30,13 +30,49 @@ System.out.println(map.get("crew").toString());
 
 
     }
+
+
+
+            public static String run(String[] product_names, int[] quantities, int[] prices, float discount, float delivery_fee) {
+                /*
+                 * Write your code below; return type and arguments should be according to the problem's requirements
+                 */
+
+
+                String output = "";
+        for (int i = 0; i < product_names.length; i++) {
+            output += product_names[i] + " " + quantities[i] + " " + prices[i] + " " + discount + " " + delivery_fee + " " + (quantities[i] * prices[i] * (1 - discount / 100) + delivery_fee) + "\n";
+        }
+                return output;
+            }
+
+
+
+
+
+
     @Test
-    public void test2() {
-       Driver.get().get("https://www.google.com");
-        System.out.println(Driver.get().getTitle());
-        Driver.closeDriver();
+public void test2() {
+    /*
+    * product_names = ["Apples", "Oranges", "Bananas"]
+quantities = [5, 3, 10]
+prices = [1, 2, 1]
+discount = 10
+delivery_fee = 5
+*
+* output = "Apples: 9.50\nOranges: 10.40\nBananas: 14.00"
+    * */
 
-
-
+    String[] product_names = {"Apples", "Oranges", "Bananas"};
+    int[] quantities = {5, 3, 10};
+    int[] prices = {1, 2, 1};
+    float discount = 10;
+    float delivery_fee = 5;
+    String output = "";
+    //output = "Apples: 9.50\nOranges: 10.40\nBananas: 14.00" should be the output
+    for (int i = 0; i < product_names.length; i++) {
+        output += product_names[i]+" " +(quantities[i] * prices[i] * (1 - discount / 100) + delivery_fee) + "/n";
     }
-}
+    System.out.println(output);
+
+}}
